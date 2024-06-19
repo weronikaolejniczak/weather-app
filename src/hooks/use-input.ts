62df2948227f1534d339577374bdb2b79699e6e8
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-06-19 22:56:14 +0200

    test: fetch and display weather on the dashboard on debounced search

--- src/hooks/use-input.ts
+++ src/hooks/use-input.ts
@@ -1,7 +1,7 @@
 import { ChangeEventHandler, useState } from 'react';
 
-export const useInput = () => {
-  const [value, setValue] = useState('');
+export const useInput = (initialValue: string = '') => {
+  const [value, setValue] = useState(initialValue);
 
   const onChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     setValue(event.currentTarget.value);
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,28 +1,99 @@
+import { useMemo, useState } from 'react';
+
 import { useWeather } from '../api/weather';
 import { useDebounce } from '../hooks/use-debounce';
 import { useInput } from '../hooks/use-input';
 import { formatDate } from '../utils/format-date';
 
-export const Dashboard = () => {
-  const [searchQuery, handleSearchQueryChange] = useInput();
+const DEFAULT_SEARCH_QUERY = 'Barcelona';
+const SEARCH_DEBOUNCE_VALUE = 500;
 
-  const debouncedQuery = useDebounce(searchQuery, 1000);
-  const query = useWeather(debouncedQuery);
+export const Dashboard = () => {
+  const [searchQuery, handleSearchQueryChange] = useInput(DEFAULT_SEARCH_QUERY);
+  const [isSearchInputFocused, setIsSearchInputFocused] = useState(false);
+
+  const debouncedQuery = useDebounce(searchQuery, SEARCH_DEBOUNCE_VALUE);
+  const { weather, savedQueries, error, isLoading, isError } =
+    useWeather(debouncedQuery);
+
+  const autofillOptions = useMemo(
+    () => savedQueries.filter((query) => query !== debouncedQuery),
+    [debouncedQuery, savedQueries],
+  );
 
   return (
     <div>
       <main>
+        <div>logo</div>
+        {weather && (
+          <div>
+            <span>icon</span>
+            <span>{weather.main.temp}°C</span>
+          </div>
+        )}
         <div>
-          <span>{query.data?.main?.temp}</span>
-        </div>
-        <div>
-          <h1>{query.data?.name}</h1>
-          {query.data?.dt && <p>{formatDate(new Date(query.data?.dt))}</p>}
+          <h1>{weather?.name}</h1>
+          {weather?.dt && <p>{formatDate(new Date(weather?.dt))}</p>}
         </div>
       </main>
       <aside>
-        <input value={searchQuery} onChange={handleSearchQueryChange} />
-        <div></div>
+        <label>
+          <span>City</span>
+          <input
+            value={searchQuery}
+            onChange={handleSearchQueryChange}
+            onFocus={() => setIsSearchInputFocused(true)}
+            onBlur={() => setIsSearchInputFocused(false)}
+          />
+          {isSearchInputFocused && !!autofillOptions.length && (
+            <menu role="menu" aria-label="Previously searched cities">
+              {autofillOptions.map((query) => (
+                <li role="menuitem" key={query}>
+                  {query}
+                </li>
+              ))}
+            </menu>
+          )}
+          {isLoading && <span>Loading...</span>}
+          {isError && error?.response.status === 404 && (
+            <span>This city doesn't exist!</span>
+          )}
+        </label>
+        {weather && (
+          <div>
+            <div>
+              <span>{weather.weather[0].description}</span>
+              <div>
+                <span>Max</span>
+                <div>
+                  <span>{weather?.main.temp_max}°C</span>
+                  <span>icon</span>
+                </div>
+              </div>
+              <div>
+                <span>Min</span>
+                <div>
+                  <span>{weather?.main.temp_min}°C</span>
+                  <span>icon</span>
+                </div>
+              </div>
+              <div>
+                <span>Humidity</span>
+                <div>
+                  <span>{weather?.main.humidity}%</span>
+                  <span>icon</span>
+                </div>
+              </div>
+              <div>
+                <span>Wind</span>
+                <div>
+                  <span>{weather?.wind.speed}km/h</span>
+                  <span>icon</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </aside>
     </div>
   );
