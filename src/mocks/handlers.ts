ed4ddaf46d24a41976d658d5a47de098438d1808
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-06-19 22:51:54 +0200

    chore: add weather endpoint mocks and create an msw handler

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,1 +1,28 @@
-export const handlers = [];
+import { http, HttpResponse } from 'msw';
+
+import { Endpoint } from '../api/endpoints';
+import successBarcelona from '../mocks/weather/success-response-barcelona.json';
+import successParis from '../mocks/weather/success-response-paris.json';
+import successWarsaw from '../mocks/weather/success-response-warsaw.json';
+import errorResponse404 from '../mocks/weather/error-response-404.json';
+
+const MOCKS = {
+  Barcelona: successBarcelona,
+  Paris: successParis,
+  Warsaw: successWarsaw,
+};
+
+type City = keyof typeof MOCKS;
+
+export const handlers = [
+  http.get(Endpoint.weather, ({ request }) => {
+    const url = new URL(request.url);
+    const city = url.searchParams.get('q');
+
+    if (!city) throw new Error('City has not been passed in query parameters!');
+    if (!(city in MOCKS))
+      return HttpResponse.json(errorResponse404, { status: 404 });
+
+    return HttpResponse.json(MOCKS[city as City]);
+  }),
+];
