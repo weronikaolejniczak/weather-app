5f1c1826f709e048d457cd0bd93961b7ce12362c
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-06-19 22:53:44 +0200

    feat: add hook for handling local storage

---
+++ src/hooks/use-persistent-state.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export const usePersistentState = <T>(key: string, initialValue: T) => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T | ((value: T) => T)) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+
+      setStoredValue(valueToStore);
+
+      localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue] as const;
+};
