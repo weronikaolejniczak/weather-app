415ff4b2033f3d3ad4527041b42e945a4c39ab47
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-06-21 21:31:59 +0200

    feat: add card width spacing to Tailwind extends config

--- tailwind.config.js
+++ tailwind.config.js
@@ -66,6 +66,9 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      spacing: {
+        128: '27.5rem',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
