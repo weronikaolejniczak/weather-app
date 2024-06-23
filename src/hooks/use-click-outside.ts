6274044452e97cfe1adbae43c6d3929331b2be1b
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-06-23 19:21:05 +0200

    feat: add a custom hook for clicking outside of element

---
+++ src/hooks/use-click-outside.ts
@@ -0,0 +1,22 @@
+import { RefObject, useEffect } from 'react';
+
+const useClickOutside = <T extends HTMLElement>(
+  ref: RefObject<T>,
+  callback: () => void,
+) => {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref, callback]);
+};
+
+export { useClickOutside };
