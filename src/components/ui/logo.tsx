0e21d69dec4f711a652c100ae32c58392dcc4ab2
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-06-23 21:14:51 +0200

    feat: allow for a className in the Logo component

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,6 +1,17 @@
-const Logo = () => {
+import { cn } from '@/utils/cn';
+
+interface Props {
+  className?: string;
+}
+
+const Logo = ({ className }: Props) => {
   return (
-    <div className="border border-black/100 text-4xl uppercase w-fit px-6 py-2">
+    <div
+      className={cn(
+        'border border-black/100 text-4xl uppercase w-fit px-6 py-2',
+        className,
+      )}
+    >
       logo
     </div>
   );
