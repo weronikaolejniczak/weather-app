4cd96cdb09e45ed2a082a3859bd360bf5c837443
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-06-23 21:12:15 +0200

    feat: allow for an icon in Input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,20 +3,25 @@ import * as React from 'react';
 import { cn } from '@/utils/cn';
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  icon?: React.ReactNode;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, icon, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          'flex px-2 h-12 w-full border-0 border-b border-solid border-black bg-transparent text-lg placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
-          className,
-        )}
-        ref={ref}
-        {...props}
-      />
+      <div className="flex items-center gap-6 placeholder:w-full h-12 border-0 border-b border-solid border-black">
+        <input
+          type={type}
+          className={cn(
+            'px-2 bg-transparent text-lg placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        {icon}
+      </div>
     );
   },
 );
