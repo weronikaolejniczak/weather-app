6cd394cc1a1dbc2401bc6c86657565dbd6857f3a
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-06-24 19:34:19 +0200

    chore: mock forecast endpoint

--- src/mocks/forecast/index.ts
+++ src/mocks/forecast/index.ts
@@ -2,8 +2,10 @@ import { HttpResponse, http } from 'msw';
 
 import { Endpoint } from '@/api/endpoints';
 
+import defaultResponse from './default.json';
+
 export const handlers = [
   http.get(Endpoint.forecast, () => {
-    return HttpResponse.json();
+    return HttpResponse.json(defaultResponse);
   }),
 ];
